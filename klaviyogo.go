e3af9ebddfc8cb226a0a9417a3e08db6e6a02f8b
Thomas <thomas.sarpong@yum.com>
2024-02-24 23:35:16 -0500

    Updated main klaviyo api

--- klaviyogo.go
+++ klaviyogo.go
@@ -2,12 +2,14 @@ package klaviyo
 
 import (
 	accounts "github.com/developertom01/klaviyo-go/api/accountsApi"
+	campaigns "github.com/developertom01/klaviyo-go/api/campaignsApi"
 	"github.com/developertom01/klaviyo-go/common"
 	"github.com/developertom01/klaviyo-go/options"
 )
 
 type KlaviyoApi struct {
-	Accounts accounts.AccountsApi
+	Accounts  accounts.AccountsApi
+	Campaigns campaigns.CampaignsApi
 }
 
 func NewKlaviyoApi(apiKey string, retryOption *common.RetryOptions) *KlaviyoApi {
@@ -15,6 +17,7 @@ func NewKlaviyoApi(apiKey string, retryOption *common.RetryOptions) *KlaviyoApi 
 	session := common.NewApiKeySession(opt, retryOption)
 
 	return &KlaviyoApi{
-		Accounts: accounts.NewAccountsApi(session, nil),
+		Accounts:  accounts.NewAccountsApi(session, nil),
+		Campaigns: campaigns.NewCampaignsApi(session, nil),
 	}
 }
