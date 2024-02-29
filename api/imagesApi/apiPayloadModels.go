cfdbab03c401298ac92d415cca19aa2aed22fd09
Thomas <thomas.sarpong@yum.com>
2024-02-29 09:52:58 -0500

    ..

--- api/imagesApi/apiPayloadModels.go
+++ api/imagesApi/apiPayloadModels.go
@@ -1,9 +1,6 @@
 package images
 
-import "os"
-
 type UploadImageFromFilePayload struct {
-	File   os.File `json:"file"`             //The image file to upload. Supported image formats: jpeg,png,gif. Maximum image size: 5MB.
 	Name   *string `json:"name,omitempty"`   //A name for the image. Defaults to the filename if not provided. If the name matches an existing image, a suffix will be added.
 	Hidden *bool   `json:"hidden,omitempty"` //If true, this image is not shown in the asset library.
 }
--- api/imagesApi/images.go
+++ api/imagesApi/images.go
@@ -1,8 +1,12 @@
 package images
 
 import (
+	"bytes"
 	"context"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
 
 	"github.com/developertom01/klaviyo-go/common"
 	"github.com/developertom01/klaviyo-go/models"
@@ -12,7 +16,7 @@ type (
 	ImagesApi interface {
 		//Upload an image from a file.
 		//If you want to import an image from an existing url or a data uri, use the Upload Image From URL endpoint instead.
-		UploadImage(ctx context.Context, payload UploadImageFromFilePayload) (*models.ImageResponse, error)
+		UploadImage(ctx context.Context, fileByte *os.File, payload UploadImageFromFilePayload) (*models.ImageResponse, error)
 	}
 
 	imageApi struct {
@@ -38,6 +42,25 @@ func NewImagesApi(session common.Session, httpClient common.HTTPClient) ImagesAp
 		httpClient: client}
 }
 
-func (api *imageApi) UploadImage(ctx context.Context, payload UploadImageFromFilePayload) (*models.ImageResponse, error) {
-	panic("")
+func (api *imageApi) UploadImage(ctx context.Context, file *os.File, payload UploadImageFromFilePayload) (*models.ImageResponse, error) {
+
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	// Create a form file field
+	fileWriter, err := writer.CreateFormFile("file", os.TempDir())
+	if err != nil {
+		return nil, err
+	}
+
+	// Write the file data to the form file field
+	_, err = io.Copy(fileWriter, file)
+	if err != nil {
+		return nil, err
+	}
+
+	// Close the multipart writer
+	writer.Close()
 }
